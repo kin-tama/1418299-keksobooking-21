50c2047eec9db2289e2723eb1a181edbc154e755
kin-tama <65250935+kin-tama@users.noreply.github.com>
2020-10-24 14:55:38 +0300

    сделал module3-task2 по личному проекту

--- js/main.js
+++ js/main.js
@@ -157,30 +157,3 @@ for (let i = 0; i < ADS_NUMBER; i++) {
 }
 
 mapPins.appendChild(fragmentTwo);
-
-
-
-{/* <template id="card">
-    <article class="map__card popup">
-      <img src="img/avatars/user01.png" class="popup__avatar" width="70" height="70" alt="Аватар пользователя">
-      <button type="button" class="popup__close">Закрыть</button>
-      <h3 class="popup__title">Уютное гнездышко для молодоженов</h3>
-      <p class="popup__text popup__text--address">102-0082 Tōkyō-to, Chiyoda-ku, Ichibanchō, 14−3</p>
-      <p class="popup__text popup__text--price">5200&#x20bd;<span>/ночь</span></p>
-      <h4 class="popup__type">Квартира</h4>
-      <p class="popup__text popup__text--capacity">2 комнаты для 3 гостей</p>
-      <p class="popup__text popup__text--time">Заезд после 14:00, выезд до 10:00</p>
-      <ul class="popup__features">
-        <li class="popup__feature popup__feature--wifi"></li>
-        <li class="popup__feature popup__feature--dishwasher"></li>
-        <li class="popup__feature popup__feature--parking"></li>
-        <li class="popup__feature popup__feature--washer"></li>
-        <li class="popup__feature popup__feature--elevator"></li>
-        <li class="popup__feature popup__feature--conditioner"></li>
-      </ul>
-      <p class="popup__description">Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.</p>
-      <div class="popup__photos">
-        <img src="" class="popup__photo" width="45" height="40" alt="Фотография жилья">
-      </div>
-    </article>
-  </template> */}
