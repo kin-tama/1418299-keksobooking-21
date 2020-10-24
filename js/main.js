bd769828305851c836c6bc8fa9de1dd6402f00e4
kin-tama <65250935+kin-tama@users.noreply.github.com>
2020-10-24 14:53:25 +0300

    сделал module3-task2 по личному проекту

--- js/main.js
+++ js/main.js
@@ -6,9 +6,14 @@
 const ADS_NUMBER = 8;
 const TYPE = ["palace", "flat", "house", "bungalow"];
 const TIME = ["12:00", "13:00", "14:00"];
-const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+const FEATURES = [" wifi", " dishwasher", " parking", " washer", " elevator", " conditioner"];
 const PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
-
+const APARTS = {
+  flat: "Квартира",
+  bungalow: "Бунгало",
+  house: "Дом",
+  palace: "Дворец"
+};
 
 // функция-рандомайзер
 const getRandomInteger = function (min, max) {
@@ -47,7 +52,7 @@ let getAuthors = function (number) {
   return authors;
 };
 
-getAuthors(8);
+getAuthors(ADS_NUMBER);
 
 // features
 
@@ -120,7 +125,7 @@ getSomeAdds(ADS_NUMBER);
 
 let fragment = document.createDocumentFragment();
 
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < ADS_NUMBER; i++) {
   let pinElement = pinTemplate.cloneNode(true);
   pinElement.style = "left:" + (ads[i].location.x + 40) + "px; top: " + (ads[i].location.y + 20) + "px;";
   pinElement.querySelector("img").src = ads[i].author.avatar;
@@ -129,3 +134,53 @@ for (let i = 0; i < 8; i++) {
 }
 
 mapPins.appendChild(fragment);
+
+// module3-task2
+// вставляем в mapPins (как на строке 131)
+
+let cardTemplate = document.querySelector("#card").content.querySelector(".popup");
+let fragmentTwo = document.createDocumentFragment();
+
+for (let i = 0; i < ADS_NUMBER; i++) {
+  let cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector(".popup__title").textContent = ads[i].offer.title;
+  cardElement.querySelector(".popup__text--address").textContent = ads[i].offer.address;
+  cardElement.querySelector(".popup__text--price").textContent = ads[i].offer.price + "₽/ночь";
+  cardElement.querySelector(".popup__type").textContent = APARTS[ads[i].offer.type];
+  cardElement.querySelector(".popup__text--capacity").textContent = ads[i].offer.rooms + " комнаты для " + ads[i].offer.guests + " гостей";
+  cardElement.querySelector(".popup__text--time").textContent = "заезд после " + ads[i].offer.checkin + ", выезд до " + ads[i].offer.checkout;
+  cardElement.querySelector(".popup__features").textContent = String(ads[i].offer.features);
+  cardElement.querySelector(".popup__description").textContent = ads[i].offer.description;
+  // cardElement.querySelector(".popup__photos").appendChild(img).src = ads[i].offer.photos; не совсем понял, что и куда конкретно нужно вставлять - поправлю в следующей итерации
+  cardElement.querySelector(".popup__avatar").src = ads[i].author.avatar;
+  fragmentTwo.appendChild(cardElement);
+}
+
+mapPins.appendChild(fragmentTwo);
+
+
+
+{/* <template id="card">
+    <article class="map__card popup">
+      <img src="img/avatars/user01.png" class="popup__avatar" width="70" height="70" alt="Аватар пользователя">
+      <button type="button" class="popup__close">Закрыть</button>
+      <h3 class="popup__title">Уютное гнездышко для молодоженов</h3>
+      <p class="popup__text popup__text--address">102-0082 Tōkyō-to, Chiyoda-ku, Ichibanchō, 14−3</p>
+      <p class="popup__text popup__text--price">5200&#x20bd;<span>/ночь</span></p>
+      <h4 class="popup__type">Квартира</h4>
+      <p class="popup__text popup__text--capacity">2 комнаты для 3 гостей</p>
+      <p class="popup__text popup__text--time">Заезд после 14:00, выезд до 10:00</p>
+      <ul class="popup__features">
+        <li class="popup__feature popup__feature--wifi"></li>
+        <li class="popup__feature popup__feature--dishwasher"></li>
+        <li class="popup__feature popup__feature--parking"></li>
+        <li class="popup__feature popup__feature--washer"></li>
+        <li class="popup__feature popup__feature--elevator"></li>
+        <li class="popup__feature popup__feature--conditioner"></li>
+      </ul>
+      <p class="popup__description">Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.</p>
+      <div class="popup__photos">
+        <img src="" class="popup__photo" width="45" height="40" alt="Фотография жилья">
+      </div>
+    </article>
+  </template> */}
